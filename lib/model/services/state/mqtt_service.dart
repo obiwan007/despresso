77e03ec3ae0f9b659721a36b3f6576dbcd0c7d7f
randomcoffeesnob <ydietler@gmail.com>
2023-01-31 19:51:33 +0100

    Clean Up MQTT service - still not working

--- lib/model/services/state/mqtt_service.dart
+++ lib/model/services/state/mqtt_service.dart
@@ -1,92 +1,103 @@
 import 'dart:async';
 import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:mqtt_client/mqtt_client.dart';
-import 'package:mqtt_client/mqtt_server_client.dart';
 import 'settings_service.dart';
 
 final mqttServer = SettingsService().mqttServer;
 final mqttPort = SettingsService().mqttPort;
 final mqttEnabled = SettingsService().mqttEnabled;
+final mqttUser = SettingsService().mqttUser;
+final mqttPassword = SettingsService().mqttPassword;
 
-final client = MqttServerClient(mqttServer, mqttPort.toString());
+final client = MqttClient(mqttServer, "");
+// final client = MqttServerClient(mqttServer, mqttPort.toString());
 
-class MqttService {
+class MqttService extends ChangeNotifier {
+  late SettingsService settingsService;
+
   MqttService() {
+    debugPrint('init mqtt');
     startService();
   }
 
   Future<int> startService() async {
-    client.logging(on: true);
-    client.keepAlivePeriod = 60;
-    client.onDisconnected = onDisconnected;
-    client.onConnected = onConnected;
-    client.onSubscribed = onSubscribed;
-    client.pongCallback = pong;
+    if (mqttEnabled) {
+      client.port = mqttPort;
+      debugPrint('mqtt enabled starting service');
+      client.logging(on: true);
+      debugPrint('mqtt service started');
+      client.keepAlivePeriod = 60;
+      client.onDisconnected = onDisconnected;
+      // client.onConnected = onConnected;
+      // client.onSubscribed = onSubscribed;
+      // client.pongCallback = pong;
 
-    final connMess = MqttConnectMessage()
-        .withClientIdentifier('dart_client')
-        .withWillTopic('willtopic')
-        .withWillMessage('My Will message')
-        .startClean()
-        .withWillQos(MqttQos.atLeastOnce);
-    print('Client connecting....');
-    client.connectionMessage = connMess;
+      final connMess = MqttConnectMessage()
+          .withClientIdentifier('despresso')
+          // .withWillTopic('willtopic')
+          // .withWillMessage('My Will message')
+          .startClean()
+          .withWillQos(MqttQos.atLeastOnce);
+      print('Client connecting....');
+      client.connectionMessage = connMess;
 
-    try {
-      await client.connect();
-    } on NoConnectionException catch (e) {
-      print('Client exception: $e');
-      client.disconnect();
-    } on SocketException catch (e) {
-      print('Socket exception: $e');
+      try {
+        print('trying to connect');
+        await client.connect(mqttUser, mqttPassword);
+      } on NoConnectionException catch (e) {
+        print('Client exception: $e');
+        client.disconnect();
+      } on SocketException catch (e) {
+        print('Socket exception: $e');
+        client.disconnect();
+      }
+
+      if (client.connectionStatus!.state == MqttConnectionState.connected) {
+        print('Client connected');
+      } else {
+        print(
+            'Client connection failed - disconnecting, status is ${client.connectionStatus}');
+        client.disconnect();
+        exit(-1);
+      }
+
+      const subTopic = 'despresso';
+      print('Subscribing to the $subTopic topic');
+      client.subscribe(subTopic, MqttQos.atMostOnce);
+      client.updates!.listen((List<MqttReceivedMessage<MqttMessage?>>? c) {
+        final recMess = c![0].payload as MqttPublishMessage;
+        final pt =
+            MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+        print('Received message: topic is ${c[0].topic}, payload is $pt');
+      });
+
+      client.published!.listen((MqttPublishMessage message) {
+        print(
+            'Published topic: topic is ${message.variableHeader!.topicName}, with Qos ${message.header!.qos}');
+      });
+
+      const pubTopic = 'despresso/status';
+      final builder = MqttClientPayloadBuilder();
+      builder.addString('Hello from mqtt_client');
+
+      print('Subscribing to the $pubTopic topic');
+      client.subscribe(pubTopic, MqttQos.exactlyOnce);
+
+      print('Publishing our topic');
+      client.publishMessage(pubTopic, MqttQos.exactlyOnce, builder.payload!);
+
+      print('Sleeping....');
+      await MqttUtilities.asyncSleep(80);
+
+      print('Unsubscribing');
+      client.unsubscribe(subTopic);
+      client.unsubscribe(pubTopic);
+
+      await MqttUtilities.asyncSleep(2);
+      print('Disconnecting');
       client.disconnect();
     }
-
-    if (client.connectionStatus!.state == MqttConnectionState.connected) {
-      print('Client connected');
-    } else {
-      print(
-          'Client connection failed - disconnecting, status is ${client.connectionStatus}');
-      client.disconnect();
-      exit(-1);
-    }
-
-    const subTopic = 'despresso/switch';
-    print('Subscribing to the $subTopic topic');
-    client.subscribe(subTopic, MqttQos.atMostOnce);
-    client.updates!.listen((List<MqttReceivedMessage<MqttMessage?>>? c) {
-      final recMess = c![0].payload as MqttPublishMessage;
-      final pt =
-          MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
-      print('Received message: topic is ${c[0].topic}, payload is $pt');
-    });
-
-    client.published!.listen((MqttPublishMessage message) {
-      print(
-          'Published topic: topic is ${message.variableHeader!.topicName}, with Qos ${message.header!.qos}');
-    });
-
-    const pubTopic = 'despresso/status';
-    final builder = MqttClientPayloadBuilder();
-    builder.addString('Hello from mqtt_client');
-
-    print('Subscribing to the $pubTopic topic');
-    client.subscribe(pubTopic, MqttQos.exactlyOnce);
-
-    print('Publishing our topic');
-    client.publishMessage(pubTopic, MqttQos.exactlyOnce, builder.payload!);
-
-    print('Sleeping....');
-    await MqttUtilities.asyncSleep(80);
-
-    print('Unsubscribing');
-    client.unsubscribe(subTopic);
-    client.unsubscribe(pubTopic);
-
-    await MqttUtilities.asyncSleep(2);
-    print('Disconnecting');
-    client.disconnect();
-
     return 0;
   }
 
