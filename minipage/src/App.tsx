d2f0e0e21563c877a510566f6efcac3d42548d9a
Markus Miertschink <markus.miertschink@hotmail.com>
2023-02-25 15:01:25 +0100

    Added material-ui with basic appbar

--- minipage/src/App.tsx
+++ minipage/src/App.tsx
@@ -1,25 +1,103 @@
 import React from 'react';
 import logo from './assets/logo.png';
 import './App.css';
+import Button from '@mui/material/Button';
+import { AppBar, Box, Container, Grid, IconButton, Paper, styled, Toolbar, Typography } from '@mui/material';
+import { color } from '@mui/system';
+import MenuIcon from '@mui/icons-material/Menu';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
+
+const bull = (
+    <Box
+        component="span"
+        sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+        •
+    </Box>
+);
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        despresso
+                    </Typography>
+                    {/* <Button color="inherit">Login</Button> */}
+                </Toolbar>
+            </AppBar>
+            <br></br>
+            <Container>
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <Card sx={{ minWidth: 275 }}>
+                            <CardContent>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                    Word of the Day
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    be{bull}nev{bull}o{bull}lent
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                    well meaning and kindly.
+                                    <br />
+                                    {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Card sx={{ minWidth: 275 }}>
+                            <CardContent>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                    Word of the Day
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    be{bull}nev{bull}o{bull}lent
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                    well meaning and kindly.
+                                    <br />
+                                    {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
     );
 }
 
