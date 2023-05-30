e1b02e91c77564dbd64b78fb6fd3c9e2bedd76eb
randomcoffeesnob <ydietler@gmail.com>
2023-05-30 20:58:22 +0200

    Added files for microbalance

---
+++ lib/devices/difluid_scale.dart
@@ -0,0 +1,162 @@
+import 'dart:async';
+import 'package:collection/collection.dart';
+import 'package:despresso/devices/abstract_comm.dart';
+import 'dart:typed_data';
+
+import 'package:despresso/devices/abstract_scale.dart';
+import 'package:despresso/model/services/ble/scale_service.dart';
+import 'package:despresso/service_locator.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+
+import 'package:logging/logging.dart' as l;
+
+class DifluidScale extends ChangeNotifier implements AbstractScale {
+  final log = l.Logger('DifluidScale');
+
+  // ignore: non_constant_identifier_names
+  static Uuid ServiceUUID =
+      useLongCharacteristics() ? Uuid.parse('0000ffe0-0000-1000-8000-00805f9b34fb') : Uuid.parse('ffe0');
+  // ignore: non_constant_identifier_names
+  static Uuid DataUUID =
+      useLongCharacteristics() ? Uuid.parse('0000ffe1-0000-1000-8000-00805f9b34fb') : Uuid.parse('ffe1');
+
+  late ScaleService scaleService;
+
+  // static const int minBattLevel = 129;
+  // static const int maxBattLevel = 158;
+
+  // static const int cmdStartTimer = 0x52;
+  // static const int cmdStopTimer = 0x53;
+  // static const int cmdResetTimer = 0x43;
+  // static const int cmdToggleTimer = 0x42;
+  // static const int cmdTogglePrecision = 0x44;
+  // static const int cmdTare = 0x54;
+  // static const int cmdToggleUnit = 0x55;
+
+  final DiscoveredDevice device;
+
+  List<int> commandBuffer = [];
+
+  late StreamSubscription<ConnectionStateUpdate> _deviceListener;
+
+  late StreamSubscription<List<int>> _characteristicsSubscription;
+  DeviceCommunication connection;
+  DifluidScale(this.device, this.connection) {
+    scaleService = getIt<ScaleService>();
+    scaleService.setScaleInstance(this);
+    _deviceListener = connection.connectToDevice(id: device.id).listen((connectionState) {
+      _onStateChange(connectionState.connectionState);
+    }, onError: (Object error) {
+      // Handle a possible error
+    });
+  }
+
+  void _notificationCallback(List<int> data) {
+    if (data.length == 13) {
+      var weight = int.parse(data.slice(0, 3).map((value) => value - 48).join(''));
+      scaleService.setWeight(weight / 10);
+      //   // scaleService.setBattery(((data[15] - minBattLevel) / (maxBattLevel - minBattLevel) * 100).round());
+    }
+  }
+
+  @override
+  writeTare() {
+    return writeToDifluidScale([0]);
+  }
+
+  Future<void> startTimer() {
+    return writeToDifluidScale([0]);
+  }
+
+  Future<void> stopTimer() {
+    return writeToDifluidScale([0]);
+  }
+
+  Future<void> resetTimer() {
+    return writeToDifluidScale([0]);
+  }
+
+  Future<void> writeToDifluidScale(List<int> payload) async {
+    log.info("Sending to Difluid Scale");
+    final characteristic =
+        QualifiedCharacteristic(serviceId: ServiceUUID, characteristicId: DataUUID, deviceId: device.id);
+    return await connection.writeCharacteristicWithoutResponse(characteristic, value: Uint8List.fromList(payload));
+  }
+
+  void _onStateChange(DeviceConnectionState state) async {
+    log.info('SCALE State changed to $state');
+
+    switch (state) {
+      case DeviceConnectionState.connecting:
+        log.info('Connecting');
+        scaleService.setState(ScaleState.connecting);
+        break;
+
+      case DeviceConnectionState.connected:
+        log.info('Connected');
+        scaleService.setState(ScaleState.connected);
+        // await device.discoverAllServicesAndCharacteristics();
+        final characteristic =
+            QualifiedCharacteristic(serviceId: ServiceUUID, characteristicId: DataUUID, deviceId: device.id);
+
+        _characteristicsSubscription = connection.subscribeToCharacteristic(characteristic).listen((data) {
+          _notificationCallback(data);
+        }, onError: (dynamic error) {
+          log.severe("Subscribe to $characteristic failed: $error");
+        });
+
+        return;
+      case DeviceConnectionState.disconnected:
+        scaleService.setState(ScaleState.disconnected);
+        scaleService.setBattery(0);
+        log.info('Difluid Scale disconnected. Destroying');
+        // await device.disconnectOrCancelConnection();
+        _characteristicsSubscription.cancel();
+
+        _deviceListener.cancel();
+        notifyListeners();
+        return;
+      default:
+        return;
+    }
+  }
+
+  @override
+  Future<void> timer(TimerMode start) async {
+    try {
+      switch (start) {
+        case TimerMode.reset:
+          await resetTimer();
+          break;
+        case TimerMode.start:
+          await resetTimer();
+          await startTimer();
+          break;
+        case TimerMode.stop:
+          await stopTimer();
+          break;
+      }
+    } catch (e) {
+      log.severe("timer failed $e");
+    }
+  }
+
+  @override
+  Future<void> beep() {
+    // TODO: implement beep
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> display(DisplayMode start) {
+    // TODO: implement display
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> power(PowerMode start) {
+    // TODO: implement power
+    throw UnimplementedError();
+  }
+}
